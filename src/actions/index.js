d63feab551c9e826b777cf43e4790eefa767cd0e
Thom Allen <thomwork@gmail.com>
2017-07-25 15:01:55 +0100

    Act crs to arrow funcs w/ object expression replacing return blocks

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,14 @@
 import { ADD_TODO, REMOVE_TODO, TOGGLE_TODO } from './../constants';
 
 // action creators
-export function addTodo(text) {
-  return { type: ADD_TODO, text };
-}
+export const addTodo = (text) => ({
+   type: ADD_TODO, text,
+});
 
-export function toggleTodo(id) {
-  return { type: TOGGLE_TODO, id };
-}
+export const toggleTodo = (id) => ({
+  type: TOGGLE_TODO, id,
+});
 
-export function removeTodo(id) {
-  return { type: REMOVE_TODO, id };
-}
+export const removeTodo = (id) => ({
+  type: REMOVE_TODO, id,
+});
