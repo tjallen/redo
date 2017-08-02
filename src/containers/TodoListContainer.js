fdd7b4efc8cf56115de835eadf1d7c8c4b2dc4b2
Thom Allen <thomwork@gmail.com>
2017-08-02 17:08:24 +0100

    TodoListContainer: getVisibleTodos => switch statement

--- src/containers/TodoListContainer.js
+++ src/containers/TodoListContainer.js
@@ -3,10 +3,18 @@ import { removeTodo, toggleTodo } from './../actions';
 import TodoList from './../components/TodoList';
 
  // placeholders til we reimplement toggling visibility / removing
-const getVisibleTodos = todos => todos;
+const getVisibleTodos = (todos, filter) => {
+  switch (filter) {
+    case 'DISPLAY_ALL': return todos;
+    case 'DISPLAY_ACTIVE': break; // TODO filter by !completed
+    case 'DISPLAY_COMPLETED': break; // TODO filter by completed
+    default:
+      return todos; // throw err once filters are implemented
+  }
+};
 
 const mapStateToProps = state => ({
-  todos: getVisibleTodos(state.todos),
+  todos: getVisibleTodos(state.todos, state.settings.filter),
 });
 
 const mapDispatchToProps = (dispatch) => {
