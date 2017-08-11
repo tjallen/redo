576ffc4e4e9fcc30afffcd17b3409a4029fa21ac
Thom Allen <thomwork@gmail.com>
2017-08-11 15:33:09 +0100

    Add tests for todos reducer - initialState + ADD_TODO action

---
+++ src/reducers/todos.spec.js
@@ -0,0 +1,32 @@
+import reducer from './todos';
+import * as types from '../constants';
+
+jest.mock('uuid', () => {
+  return {
+    v4: jest.fn(() => 0)
+  };
+});
+
+describe('todos reducer', () => {
+  it('should return initial state', () => {
+    const expected = [
+        { id: 0, text: 'foo', editing: false, completed: false },
+        { id: 0, text: 'bar', editing: false, completed: true },
+        { id: 0, text: 'baz', editing: false, completed: false },
+    ];
+    expect(reducer(undefined, {})).toEqual(expected);
+  });
+  it('should handle recieving ADD_TODO action', () => {
+    expect(
+      reducer([], {
+        type: types.ADD_TODO, text: 'foo', id: 0
+      })
+    ).toEqual([
+      {
+        id: 0,
+        text: 'foo',
+        completed: false,
+      }
+    ]);
+  });
+});
