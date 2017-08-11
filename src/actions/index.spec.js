f22d471cce507bf518c64ce5a049bc96a01f9767
Thom Allen <thomwork@gmail.com>
2017-08-11 12:01:02 +0100

    Mock uuid.v4() fn so actions/addTodo test passes

--- src/actions/index.spec.js
+++ src/actions/index.spec.js
@@ -1,12 +1,19 @@
 import * as types from '../constants';
 import * as actions from './index';
 
+jest.mock('uuid', () => {
+  return {
+    v4: jest.fn(() => 1)
+  };
+});
+
 describe('actions', () => {
   it('should create an ADD_TODO action', () => {
     const text = 'foo';
     const expected = {
       type: types.ADD_TODO,
       text,
+      id: 1,
     };
     expect(actions.addTodo(text)).toEqual(expected);
   });
