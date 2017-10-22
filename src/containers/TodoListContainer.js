f929b20ba12c3d282dc0eecf4e808ea2ba0b7388
Thom Allen <thomwork@gmail.com>
2017-10-22 11:12:18 +0100

    Fetch data from fake api on route change

--- src/containers/TodoListContainer.js
+++ src/containers/TodoListContainer.js
@@ -1,25 +1,43 @@
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { removeTodo, editTodo, toggleTodo } from './../actions';
 import TodoList from './../components/TodoList';
 import { withRouter } from 'react-router-dom';
 import { getVisibleTodos } from '../reducers';
+import { fetchTodos } from '../api';
 
-const mapStateToProps = (state, { match }) => {
-  return {
-    todos: getVisibleTodos(
-      state,
-      match.params.filter || 'all'
+class TodoListContainer extends Component {
+  componentDidMount() {
+    fetchTodos(this.props.filter).then(todos =>
+      console.log(this.props.filter, todos)
     )
+  }
+  componentDidUpdate(prevProps) {
+    if (this.props.filter !== prevProps.filter) {
+      fetchTodos(this.props.filter).then(todos =>
+        console.log(this.props.filter, todos)
+      )
+    }
+  }
+  render() {
+    return <TodoList {...this.props} />;
+  }
+}
+const mapStateToProps = (state, { match }) => {
+  const filter = match.params.filter || 'all'
+  return {
+    todos: getVisibleTodos(state, filter),
+    filter,
   }
 };
 
-const TodoListContainer = withRouter(connect(
+TodoListContainer = withRouter(connect(
   mapStateToProps,
   {
     onToggleCompletedClick: toggleTodo,
     onRemoveTodoClick: removeTodo,
     onEditTodoComplete: editTodo,
   }
-)(TodoList));
+)(TodoListContainer));
 
 export default TodoListContainer;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from './configureStore';
 import Root from './containers/Root';
-import { fetchTodos } from './api';
-
-fetchTodos('all').then(todos =>
-  console.log(todos)
-);
 
 const store = configureStore();
 
