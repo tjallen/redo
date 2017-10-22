85fc54cfd60a3524d8b99d0d1e7facd5c2a2f4e2
Thom Allen <thomwork@gmail.com>
2017-10-22 17:45:24 +0100

    Replace our promise middleware implementation with redux-promise

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,29 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
+import promise from 'redux-promise';
 import rootReducer from './reducers';
 
-const addPromiseSupportToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-  return (action) => {
-    if (typeof action.then === 'function') {
-      return action.then(rawDispatch);
-    }
-    return rawDispatch(action);
-  }
-}
-// returns a configured store instance
 const configureStore = () => {
-
-  const middleware = applyMiddleware(logger);
-  const store = createStore(
+  const middleware = [logger, promise];
+  return createStore(
     rootReducer,
-    composeWithDevTools(middleware),
+    composeWithDevTools(
+      applyMiddleware(...middleware),
+    ),
   );
-
-  store.dispatch = addPromiseSupportToDispatch(store);
-  
-  return store;
 }
 
 export default configureStore;
