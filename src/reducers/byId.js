2709f1c8bd2815eb0b6d4ce7c9c124ac0cdc752b
Thom Allen <thomwork@gmail.com>
2017-10-23 12:20:33 +0100

    Refactor the reducers
    Extract byId reducer & create new createList which returns a reducer that handles ids for the specified filter, to be combined into listByFilter (formerly idsByFilter)

---
+++ src/reducers/byId.js
@@ -0,0 +1,19 @@
+import { RECEIVE_TODOS } from '../constants';
+
+function byId(state = {}, action) {
+  switch (action.type) {
+    case RECEIVE_TODOS: {
+      const nextState = { ...state };
+      action.response.forEach(todo => {
+        nextState[todo.id] = todo;
+      });
+      return nextState;
+    }
+    default:
+      return state;
+  }
+}
+
+export default byId;
+
+export const getTodo = (state, id) => state[id];
